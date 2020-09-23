ba1d916119dc70aeb2ab013bf9e0169c51196031
Mahendiran <skymahi@github.com>
2020-09-23 21:28:40 +0530

    'initial'

---
+++ azure-devops-sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace azure_devops_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
